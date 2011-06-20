4ef4e4b8736d131da6df38506f5f4134d2048f7d
curtismchale <curtis@curtismchale.ca>
2011-06-20 14:01:18 -0700

    building out the settings pages

--- includes/admin/add-menu.php
+++ includes/admin/add-menu.php
@@ -16,13 +16,27 @@
 
     <div class="wrap">
 
-    <?php screen_icon(); ?>
-    <h2>SFNGallery Settings</h2>
+        <?php screen_icon(); ?>
+        <h2>SFNGallery Settings</h2>
+
+        <form action="options.php" method="post">
+
+        </form>
 
     </div><!-- /.wrap -->
 
 <?php
 
     }; //ends sfn_gallery_settings_page
+
+    // Register and define the settings
+    add_action('admin_init', 'sfn_gallery_admin_init');
+    function sfn_gallery_admin_init(){
+        register_setting( 'sfn_gallery_options', 'sfn_gallery_options', 'sfn_gallery_validate_options' );
+
+        add_settings_section( 'sfn_gallery_main', 'SFNGallery Settings', 'sfn_gallery_section_text', 'sfn_gallery' );
+
+        add_settings_field( 'sfn_gallery_slide_width', 'Slide Width', 'sfn_gallery_slide_width', 'sfn_gallery_options', 'sfn_gallery_main' );
+    }
 
 ?>
