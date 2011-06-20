d94840a607988f9a15e3b099b7bbe18f76e910a7
curtismchale <curtis@curtismchale.ca>
2011-06-20 13:47:25 -0700

    added menu and blank function to contain the settings page

--- includes/admin/add-menu.php
+++ includes/admin/add-menu.php
@@ -1,3 +1,16 @@
 <?php
 
+    /* adding submenu for plugin settings */
+    add_action( 'admin_menu', 'sfn_gallery_create_options_page' );
+
+    function sfn_gallery_create_options_page() {
+
+        add_options_page( 'SFNGallery Settings', 'SFNGallery Settings', 'manage_options', __FILE__, 'sfn_gallery_settings_page' );
+
+    };
+
+    function sfn_gallery_settings_page() {
+
+    };
+
 ?>
